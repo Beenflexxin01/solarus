1862b3a6b90a13647acc77a642693f612f7d88b3
Adenola Omotayo <102471748+Beenflexxin01@users.noreply.github.com>
2023-05-14 19:35:03 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict'
+const btnNav = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNav.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
